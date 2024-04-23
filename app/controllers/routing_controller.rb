260f10a7a55bacd208d7a19776a678a575aa2e27
mattmartinez45 <114887556+mattmartinez45@users.noreply.github.com>
2024-04-23 04:25:08 +0000

    more edits

--- app/controllers/routing_controller.rb
+++ app/controllers/routing_controller.rb
@@ -15,9 +15,9 @@ class RoutingController < ApplicationController
   end
 
   def divide_these
-    @first_number = params.fetch("first_num")
+    @first_number = params.fetch("first_num").to_f
     @second_number = params.fetch("second_num").to_f
-    @result =  first_number / @second_number
+    @result =  @first_number / @second_number
     render({ :template => "pages/divide_result" })
   end
 
